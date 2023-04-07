0bfa743c55ba455c86865edb7c1ad9529ce60981
LeonLow97 <lowjiewei@gmail.com>
2023-04-07 16:03:44 +0800

    Login in route

--- free_code_camp/expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/repositories/UserRepositoryImpl.java
+++ free_code_camp/expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/repositories/UserRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.pairlearning.expensetrackerapi.repositories;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
@@ -18,6 +19,8 @@ public class UserRepositoryImpl implements UserRepository {
     private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
     private static final String SQL_FIND_BY_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD " + 
                                                  "FROM ET_USERS WHERE USER_ID = ?";
+    private static final String SQL_FIND_BY_EMAIL = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD " +
+                                                    "FROM ET_USERS WHERE EMAIL = ?";
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -42,7 +45,13 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User findByEmailAndPassword(String email, String password) throws EtAuthException {
-        return null;
+        try {
+            User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, userRowMapper, email);
+            if (!password.equals(user.getPassword())) throw new EtAuthException("Invalid email/password!");
+            return user;
+        } catch (EmptyResultDataAccessException e) {
+            throw new EtAuthException("Invalid email/password");
+        }
     }
 
     @Override
