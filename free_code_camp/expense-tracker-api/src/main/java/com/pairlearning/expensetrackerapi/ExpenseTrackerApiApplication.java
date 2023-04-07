0a7d48d0b85e1c7196e2df2260896470d8b847c2
LeonLow97 <lowjiewei@gmail.com>
2023-04-07 17:13:56 +0800

    JWT Authentication

--- free_code_camp/expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/ExpenseTrackerApiApplication.java
+++ free_code_camp/expense-tracker-api/src/main/java/com/pairlearning/expensetrackerapi/ExpenseTrackerApiApplication.java
@@ -2,12 +2,24 @@ package com.pairlearning.expensetrackerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import com.pairlearning.expensetrackerapi.filters.AuthFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterResgistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
 	}
 
 }
