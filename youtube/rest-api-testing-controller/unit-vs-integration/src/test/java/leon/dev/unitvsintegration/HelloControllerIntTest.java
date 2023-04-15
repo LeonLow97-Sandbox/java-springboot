ceb5ab4cd1d1c5c43d4ef2d5c5b383a0d587c5ee
LeonLow97 <lowjiewei@gmail.com>
2023-04-15 15:18:54 +0800

    Replacing ExtendsWith(SpringExtension.class) with SpringJUnitConfig annotation

--- youtube/rest-api-testing-controller/unit-vs-integration/src/test/java/leon/dev/unitvsintegration/HelloControllerIntTest.java
+++ youtube/rest-api-testing-controller/unit-vs-integration/src/test/java/leon/dev/unitvsintegration/HelloControllerIntTest.java
@@ -1,10 +1,9 @@
 package leon.dev.unitvsintegration;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -14,7 +13,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import leon.dev.unitvsintegration.controllers.HelloController;
 import static org.junit.jupiter.api.Assertions.*;
 
-@ExtendWith(SpringExtension.class)
+@SpringJUnitConfig
 @WebMvcTest(HelloController.class)
 public class HelloControllerIntTest {
 
